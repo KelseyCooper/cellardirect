1ee32920ec8369730891eada558e1e1a877282be
Kelsey Cooper <kcoop@live.ca>
2018-04-03 18:15:47 -0700

    refactored functions into imported helperFunction file, same for ShippingRates

--- server/index.js
+++ server/index.js
@@ -18,13 +18,8 @@ const ShopifyAPIClient = require('shopify-api-node')
 const ShopifyExpress = require('@shopify/shopify-express')
 const { MemoryStrategy } = require('@shopify/shopify-express/strategies')
 
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
-
-const ENV = process.env.ENV || 'development'
-const knexConfig = require('./knexfile')
-const knex = require('knex')(knexConfig[ENV])
+const { caseAmount, findAddress, newCustomerOrder } = require('./lib/helperFunctions')
+const { shippingRates } = require('./lib/shippingRates')
 
 const {
   SHOPIFY_APP_KEY,
@@ -153,70 +148,6 @@ if (isDevelopment) {
 // Install
 app.get('/install', (req, res) => res.render('install'))
 
-function caseAmount(num) {
-  let number = Math.floor(num / 12) + 1
-  if (num % 12 === 0) {
-    number -= 1
-  }
-  return number
-}
-
-async function findAddress(rate) {
-  const { address1: address } = rate.destination
-  const data = {}
-  let orderTotal = 0
-  await rate.items.map(item => {
-    //TODO test this and see if it works
-    // if any item comes in with a quantitity of 12, it is substracted from it's total.
-    while (item.quantity >= 12) {
-      item.quantity = item.quantity - 12
-    }
-    orderTotal += item.quantity
-  })
-  return await knex('customers')
-    .select()
-    .where('address', address)
-    .returning('*')
-    .then(result => {
-      // Checks if an address exists in the system
-      if (result.length === 0) {
-        data.orderTotal = orderTotal
-        data.prePurchasedCases = 0
-        data.prePurchasedBottles = 0
-        return data
-      } else {
-        data.prePurchasedCases = caseAmount(result[0].bottles_purchased)
-        data.prePurchasedBottles = result[0].bottles_purchased
-        return knex('orders')
-          .where('customer_id', result[0].id)
-          .join('purchased_items', 'orders.id', '=', 'purchased_items.order_id')
-          .then(result => {
-            result.map(item => {
-              orderTotal += item.quantity
-            })
-            data.orderTotal = orderTotal
-            return data
-          })
-      }
-    })
-}
-
-const shippingRates = {
-  AB: [10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000, 1000],
-  BC: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  MB: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  NB: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  NL: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  NT: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  NS: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  NU: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  ON: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  PE: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  QC: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  SK: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-  YT: [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100],
-}
-
 app.post('/custom-shipping', function(req, res) {
   const { province } = req.body.rate.destination
   console.log(province);
@@ -309,94 +240,7 @@ app.get('/', withShop, function(request, response) {
   })
 })
 
-function newCustomerOrder(body) {
-  const { email, first_name: firstName, last_name: lastName } = body.customer
 
-  const { address1: address } = body.customer.default_address
-
-  // totals the amount of bottles purchased to insert or increment in the db
-  let bottlesPurchased = 0
-  body.line_items.map(item => {
-    bottlesPurchased += item.quantity
-  })
-
-  // finds a custoemr with the address used (may need to replace this)
-  return knex('customers')
-    .select()
-    .where('address', address)
-    .then(result => {
-      //if the customer doesn'y exist create one
-      if (result.length === 0) {
-        console.log('new user created')
-
-        return knex('customers')
-          .insert({
-            first_name: firstName,
-            last_name: lastName,
-            email,
-            address,
-            bottles_purchased: bottlesPurchased,
-          })
-          .returning('id')
-          .then(id => {
-            // use the id of the customer created to create orders
-            return knex('orders')
-              .insert({
-                customer_id: id[0],
-              })
-              .returning('id')
-          })
-          .then(id => {
-            //use the id of the orders to create pruchased items
-            body.line_items.map(item => {
-              return knex('purchased_items')
-                .insert({
-                  product_name: item.title,
-                  quantity: item.quantity,
-                  order_id: id[0],
-                })
-                .then(() => {
-                  return true
-                })
-            })
-            return true
-          })
-      } else {
-        //if the customer does exist, find them and increment the bottlesPurchased
-        return knex('customers')
-          .select()
-          .where('address', address)
-          .increment('bottles_purchased', bottlesPurchased)
-          .returning('id')
-          .then(result => {
-            // insert the new order
-            return knex('orders')
-              .insert({
-                customer_id: result[0],
-              })
-              .returning('id')
-              .then(id => {
-                // insert the new purchases connected to the order
-                body.line_items.map(item => {
-                  // console.log(item.title)
-                  return knex('purchased_items')
-                    .insert({
-                      product_name: item.title,
-                      quantity: item.quantity,
-                      order_id: id[0],
-                    })
-                    .then(() => {
-                      return true
-                    })
-                })
-                return true
-              })
-          })
-      }
-    })
-
-  return true
-}
 
 app.post('/order-create', function(req, res) {
   newCustomerOrder(req.body)
---
+++ server/lib/helperFunctions.js
@@ -0,0 +1,148 @@
+// const environment = process.env.NODE_ENV || 'development'
+// const configuration = require('../knexfile')[environment]
+// const database = require('knex')(configuration)
+
+const ENV = process.env.ENV || 'development'
+const knexConfig = require('../knexfile')
+const knex = require('knex')(knexConfig[ENV])
+
+function caseAmount(num) {
+  let number = Math.floor(num / 12) + 1
+  if (num % 12 === 0) {
+    number -= 1
+  }
+  return number
+}
+
+async function findAddress(rate) {
+  const { address1: address } = rate.destination
+  const data = {}
+  let orderTotal = 0
+  await rate.items.map(item => {
+    while (item.quantity >= 12) {
+      item.quantity = item.quantity - 12
+    }
+    orderTotal += item.quantity
+  })
+  return await knex('customers')
+    .select()
+    .where('address', address)
+    .returning('*')
+    .then(result => {
+      // Checks if an address exists in the system
+      if (result.length === 0) {
+        data.orderTotal = orderTotal
+        data.prePurchasedCases = 0
+        data.prePurchasedBottles = 0
+        return data
+      } else {
+        data.prePurchasedCases = caseAmount(result[0].bottles_purchased)
+        data.prePurchasedBottles = result[0].bottles_purchased
+        return knex('orders')
+          .where('customer_id', result[0].id)
+          .join('purchased_items', 'orders.id', '=', 'purchased_items.order_id')
+          .then(result => {
+            result.map(item => {
+              orderTotal += item.quantity
+            })
+            data.orderTotal = orderTotal
+            return data
+          })
+      }
+    })
+}
+
+function newCustomerOrder(body) {
+  const { email, first_name: firstName, last_name: lastName } = body.customer
+
+  const { address1: address } = body.customer.default_address
+
+  // totals the amount of bottles purchased to insert or increment in the db
+  let bottlesPurchased = 0
+  body.line_items.map(item => {
+    bottlesPurchased += item.quantity
+  })
+
+  // finds a custoemr with the address used (may need to replace this)
+  return knex('customers')
+    .select()
+    .where('address', address)
+    .then(result => {
+      //if the customer doesn'y exist create one
+      if (result.length === 0) {
+        console.log('new user created')
+
+        return knex('customers')
+          .insert({
+            first_name: firstName,
+            last_name: lastName,
+            email,
+            address,
+            bottles_purchased: bottlesPurchased,
+          })
+          .returning('id')
+          .then(id => {
+            // use the id of the customer created to create orders
+            return knex('orders')
+              .insert({
+                customer_id: id[0],
+              })
+              .returning('id')
+          })
+          .then(id => {
+            //use the id of the orders to create pruchased items
+            body.line_items.map(item => {
+              return knex('purchased_items')
+                .insert({
+                  product_name: item.title,
+                  quantity: item.quantity,
+                  order_id: id[0],
+                })
+                .then(() => {
+                  return true
+                })
+            })
+            return true
+          })
+      } else {
+        //if the customer does exist, find them and increment the bottlesPurchased
+        return knex('customers')
+          .select()
+          .where('address', address)
+          .increment('bottles_purchased', bottlesPurchased)
+          .returning('id')
+          .then(result => {
+            // insert the new order
+            return knex('orders')
+              .insert({
+                customer_id: result[0],
+              })
+              .returning('id')
+              .then(id => {
+                // insert the new purchases connected to the order
+                body.line_items.map(item => {
+                  // console.log(item.title)
+                  return knex('purchased_items')
+                    .insert({
+                      product_name: item.title,
+                      quantity: item.quantity,
+                      order_id: id[0],
+                    })
+                    .then(() => {
+                      return true
+                    })
+                })
+                return true
+              })
+          })
+      }
+    })
+
+  return true
+}
+
+module.exports = {
+  caseAmount,
+  findAddress,
+  newCustomerOrder,
+}
